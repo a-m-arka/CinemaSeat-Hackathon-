7c1d8bf24a69e818debde280292a664d7d5d3a3a
A. M. Arka <amarka310@gmail.com>
2026-08-08 13:19:23 +0600

    initialized DBr

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+
+require('dotenv').config();
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
